65be7622c5b1873d3b6ba1cf77da08bb361bda7b
Kayla <you@example.com>
2024-03-29 10:12:26 -0400

    Updating python

--- Capstone.py
+++ Capstone.py
@@ -3,23 +3,32 @@ import matplotlib.pyplot as plt
 
 complaints = pd.read_csv("COMPLAINTS SHEET 2.csv")
 
+complaints = complaints[complaints["Date received"].notna()]
+
 complaints2 = pd.read_csv("Copy of Complaints csv.csv")
 
-complaints['Complaint ID'] = complaints['Complaint ID'].astype(str)
-complaints2['Complaint ID'] = complaints2['Complaint ID'].astype(str)
+
+complaints['Complaint ID'] = complaints['Complaint ID'].astype(int).astype(str)
+complaints2['Complaint ID'] = complaints2['Complaint ID'].astype(int).astype(str)
 
 complaints["Company_ID"] = complaints["Company"] + "_" + complaints["Complaint ID"]
 
 complaints2["Company_ID"] = complaints2["Company"] + "_" + complaints2["Complaint ID"]
 
+complaints.head()
+
 all_complaints = pd.merge(complaints2, complaints, how="left", on="Company_ID")
 
 most_complaints_by_product = pd.DataFrame(all_complaints["Product"].value_counts().reset_index())
 
+import matplotlib.pyplot as plt
+
 plt.barh(most_complaints_by_product["Product"],most_complaints_by_product["count"])
+
+# It appears that the number of complaints was the highest for Credit reporting, credit repair services, or other personal consumer reports.
 plt.title("Number of Complaints by Product")
 plt.show()
-# It appears that the number of complaints was the highest for Credit reporting, credit repair services, or other personal consumer reports.
+
 
 all_complaints["Date received_x"] = pd.to_datetime(all_complaints["Date received_x"])
 
@@ -27,9 +36,10 @@ num_complaints_by_date = pd.DataFrame(all_complaints.groupby("Date received_x")[
 
 plt.figure(figsize=(20, 6))
 plt.plot(num_complaints_by_date["Date received_x"],num_complaints_by_date["Complaint ID_x"])
+# It appears that the month of April has the most complaints per month.
 plt.title("Number of Complaints by Date")
 plt.show()
-# It appears that the month of April has the most complaints per month.
+
 
 product_responses = pd.DataFrame(all_complaints.groupby("Product")["Timely response?"].value_counts().reset_index())
 product_responses_yes = product_responses[product_responses["Timely response?"] == "Yes"]
@@ -37,7 +47,6 @@ product_responses_yes = product_responses[product_responses["Timely response?"] 
 product_responses_no = product_responses[product_responses["Timely response?"] == "No"]
 
 plt.barh(product_responses_yes["Product"], product_responses_yes["count"], label = 'Yes') 
-
+# It appears that the credit reporting has the most timely responses all of the other Products.
 plt.title("Number of Timely Responses per Product")
 plt.show()
-# It appears that the credit reporting has the most timely responses all of the other Products.
